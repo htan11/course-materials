fa63663f02d4f7879f12914ff45cc0263c2c57d2
Harsh Tanwar <142919718+htan11@users.noreply.github.com>
2026-02-05 00:04:00 +0530

    Delete UCS654/Assignment1-Topsis/_topsis_entry.py

--- UCS654/Assignment1-Topsis/_topsis_entry.py
+++
@@ -1,7 +0,0 @@
-"""Entry point for the topsis console script (package name has a hyphen)."""
-from importlib import import_module
-
-
-def main():
-    mod = import_module("102303812-topsis.topsis")
-    mod.main()
